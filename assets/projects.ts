af2e1751fb7674ac83f55da59fa99b72905ee1a1
kazuya.zama <zama@restarts.co.jp>
2023-02-17 22:26:19 +0900

    hydrostore公開

--- assets/projects.ts
+++ assets/projects.ts
@@ -1,28 +1,25 @@
 export const projects = [
-    {
-      path: "/",
-      // "https://hydrostore.vercel.app/",
-      src: "/HydroStore.png",
-      title:"※改修中です",
-      //  "HydroStore",
-      use: ["typescript", "NEXT.js13", "mantineUI", "tailwindcss"],
-      descripttion:
-        "HydroStoreは日本初のshopify FW「Hydrogen」に特化したECサイト制作代行サービスです。　",
-    },
-    {
-      path: "https://mansion-wifi.net/",
-      src: "/マンションwifiネット.png",
-      title: "マンションwifiネット",
-      use: ["PHP", "Larevel", "SASS"],
-      descripttion:
-        "マンションwifiネットは建物導入済の回線を一覧で検索・比較・相談・お申し込みまで可能にした唯一のWEBサイトです。",
-    },
-    {
-      path: "https://biglobe-lp.pages.dev/",
-      src: "/ビッグローブ光LP.png",
-      title: "ビッグローブ光LPサイト",
-      use: ["typescript", "NEXT.js12", "tailwindcss"],
-      descripttion: " ビッグローブ光正規代理店の公式LPサイトです。",
-    },
-  ];
-  
+  {
+    path: "https://hydrostore.vercel.app/",
+    src: "/HydroStore.png",
+    title: "HydroStore",
+    use: ["typescript", "NEXT.js13", "mantineUI", "tailwindcss"],
+    descripttion:
+      "HydroStoreは日本初のshopify FW「Hydrogen」に特化したECサイト制作代行サービスです。　",
+  },
+  {
+    path: "https://mansion-wifi.net/",
+    src: "/マンションwifiネット.png",
+    title: "マンションwifiネット",
+    use: ["PHP", "Larevel", "SASS"],
+    descripttion:
+      "マンションwifiネットは建物導入済の回線を一覧で検索・比較・相談・お申し込みまで可能にした唯一のWEBサイトです。",
+  },
+  {
+    path: "https://biglobe-lp.pages.dev/",
+    src: "/ビッグローブ光LP.png",
+    title: "ビッグローブ光LPサイト",
+    use: ["typescript", "NEXT.js12", "tailwindcss"],
+    descripttion: " ビッグローブ光正規代理店の公式LPサイトです。",
+  },
+];
